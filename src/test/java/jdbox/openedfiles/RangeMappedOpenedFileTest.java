f3042facfe0d4214668d4db6403c4e1967a2f7ed
stepank <stepankk@gmail.com>
2015-02-22 13:21:29 +0100

    fuzzy read test for RangeMappedOpenedFile

--- src/test/java/jdbox/openedfiles/RangeMappedOpenedFileTest.java
+++ src/test/java/jdbox/openedfiles/RangeMappedOpenedFileTest.java
@@ -5,14 +5,20 @@ import jdbox.Uploader;
 import jdbox.filetree.File;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.nio.ByteBuffer;
+import java.util.Random;
 import java.util.concurrent.ScheduledExecutorService;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class RangeMappedOpenedFileTest extends BaseTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(RangeMappedOpenedFileTest.class);
 
     private final static int bufferSize = 4;
 
@@ -24,11 +30,13 @@ public class RangeMappedOpenedFileTest extends BaseTest {
     public void setUp() throws Exception {
         super.setUp();
         uploader = injector.getInstance(Uploader.class);
-        file = drive.createFile(testFileName, testDir, getTestContent());
     }
 
     @Test
     public void read() throws Exception {
+
+        file = drive.createFile(testFileName, testDir, getTestContent());
+
         runReadTest(11);
         runReadTest(16);
         runReadTest(4, 4, 3);
@@ -44,7 +52,7 @@ public class RangeMappedOpenedFileTest extends BaseTest {
     private void runReadTest(int... counts) throws Exception {
 
         openedFile = RangeMappedOpenedFile.create(
-                file, drive, uploader, injector.getInstance(ScheduledExecutorService.class), bufferSize);
+                file, drive, null, injector.getInstance(ScheduledExecutorService.class), bufferSize);
         assertThat(openedFile.getBufferCount(), equalTo(0));
 
         byte[] bytes = new byte[testContentString.length()];
@@ -69,6 +77,9 @@ public class RangeMappedOpenedFileTest extends BaseTest {
 
     @Test
     public void write() throws Exception {
+
+        file = drive.createFile(testFileName, testDir, getTestContent());
+
         runWriteTest(15);
         runWriteTest(16);
         runWriteTest(4, 4, 4, 3);
@@ -122,5 +133,46 @@ public class RangeMappedOpenedFileTest extends BaseTest {
         assertThat(openedFile.read(buffer, 0, bytes.length), equalTo(bytes.length));
         assertThat(buffer.array(), equalTo(bytes));
         assertThat(openedFile.getBufferCount(), equalTo(4));
+    }
+
+    @Test
+    public void fuzzyRead() throws Exception {
+
+        final int contentLength = 1024 * 1024;
+        final int[] maxReadChunkSizes =
+                new int[]{10, 100, 1024, 4 * 1024, 16 * 1024, 62 * 1024, 256 * 1024, contentLength};
+
+        for (int maxReadChunkSize : maxReadChunkSizes) {
+
+            logger.info("max read chunk size is {}", maxReadChunkSize);
+
+            Random random = new Random();
+            byte[] content = new byte[contentLength];
+            random.nextBytes(content);
+
+            file = drive.createFile(testFileName, testDir, new ByteArrayInputStream(content));
+
+            openedFile = RangeMappedOpenedFile.create(
+                    file, drive, null, injector.getInstance(ScheduledExecutorService.class), 1024);
+            assertThat(openedFile.getBufferCount(), equalTo(0));
+
+            for (int i = 0; i < 100; i++) {
+
+                int offset = random.nextInt(contentLength);
+                int bytesToRead = 1 + random.nextInt(Math.min(contentLength - offset - 1, maxReadChunkSize));
+
+                ByteBuffer buffer = ByteBuffer.allocate(bytesToRead);
+                assertThat(openedFile.read(buffer, offset, bytesToRead), equalTo(bytesToRead));
+
+                byte[] actual = new byte[bytesToRead];
+                buffer.rewind();
+                buffer.get(actual, 0, bytesToRead);
+
+                byte[] expected = new byte[bytesToRead];
+                System.arraycopy(content, offset, expected, 0, bytesToRead);
+
+                assertThat(actual, equalTo(expected));
+            }
+        }
     }
 }
